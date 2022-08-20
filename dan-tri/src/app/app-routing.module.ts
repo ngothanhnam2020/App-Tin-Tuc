821fd38a99362e7da4b68db23d0a7408c70ac26a
truong nguyen <19130255@st.hcmuaf.edu.vn>
2022-08-20 21:35:54 +0700

    angular_header_footer_home

--- dan-tri/src/app/app-routing.module.ts
+++ dan-tri/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailComponent } from './components/detail/detail.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'detail', component: DetailComponent},
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- dan-tri/src/app/app.module.ts
+++ dan-tri/src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { DetailComponent } from './components/detail/detail.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DetailComponent,
-    HeaderComponent,
-    FooterComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
